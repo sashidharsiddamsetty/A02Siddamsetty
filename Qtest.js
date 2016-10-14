3a2deaf97eb4e6d7f4c57c6cd268becea5151147
sashidharsiddamsetty <S528172@mail.nwmissouri.edu>
2016-10-14 00:09:56 -0500

    Delete Qtest.js

--- Qtest.js
+++
@@ -1,6 +0,0 @@
-QUnit.test('Testing Multiplication function with four sets of inputs', function (assert) {
-    assert.throws(function () { mul(); }, new Error("only numbers are allowed"), 'Passing in array correctly raises an Error');
-    assert.strictEqual(mul(1,2), 2, 'All positive numbers');
-    assert.strictEqual(mul(1,-2), -2, 'Positive and negative numbers');
-    assert.strictEqual(mul(-1,-2), 2, 'All are negative numbers');
-});
